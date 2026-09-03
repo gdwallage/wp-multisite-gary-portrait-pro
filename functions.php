2affeea3bce39e1f64d3c3c7692680daa23e218e
Gary Wallage <gary@wallage.org.uk>
2026-09-03 14:34:17 +0100

    Enforce Blacksword script font in header site title; clean up parent stylesheet enqueueing and bump version to 1.0.2

--- functions.php
+++ functions.php
@@ -5,6 +5,12 @@
  */
 
 add_action( 'wp_enqueue_scripts', function() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'), wp_get_theme()->get('Version') );
+    $parent_theme = wp_get_theme('gary-wedding-pro');
+    $parent_ver = $parent_theme->exists() ? $parent_theme->get('Version') : time();
+    $child_ver = wp_get_theme()->get('Version');
+    
+    // Ensure clean handle and cache-busting version
+    wp_dequeue_style( 'parent-style' );
+    wp_enqueue_style( 'gary-wedding-v3-editorial', get_template_directory_uri() . '/style.css', array(), $parent_ver );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('gary-wedding-v3-editorial'), $child_ver );
 }, 20 );
